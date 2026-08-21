7e44c8985cd3f29293687dce0ff2a8067f8276eb
Mridul Srivastava <srivastavamridulroman@gmail.com>
2026-08-21 12:36:04 +0530

    Pydantic and FastAPI

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class PredictionResponse(BaseModel):
 
 @app.get('/')
 def greet():
-    return {'Welcome!'}
+    return {'Welcome to Sheryians AI School Guys'}
 
 
 @app.post('/predict', response_model=PredictionResponse) #6.77777
